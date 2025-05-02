b63f8e153f62720b56c03902f2dfbc692529d88b
JoseUFV22 <9303401@alumnos.ufv.es>
2025-05-02 18:45:16 +0200

    Logger Añadido

---
+++ modulo.cs
@@ -0,0 +1,38 @@
+using System;
+using System.IO;
+
+namespace VoluntAppUtils
+{
+    public static class SimpleLogger
+    {
+        //Ruta Logs
+        private static string LogPath = "voluntapp.log";
+
+        public static void Log(string mensaje)
+        {
+            try
+            {
+                //Si no existe el log
+                if (!File.Exists(LogPath))
+                {
+                    FileStream archivoLog = File.Create(LogPath);
+                    archivoLog.Close();
+                }
+
+                //Log
+                var linea = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss} - {mensaje}\n";
+                
+                //Introduce la linea al log
+                StreamWriter writer = new StreamWriter(LogPath, append: true);
+                writer.WriteLine(linea);
+
+                writer.Close();
+            }
+
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine($"Error al escribir en el log: {ex.Message}");
+            }
+        }
+    }
+}
